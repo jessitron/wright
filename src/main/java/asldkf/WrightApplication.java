9a0dce078014af0487e9aab8fd2423190c691f2e
Jessica Kerr <jessitron@gmail.com>
2018-06-19 19:34:39 +0000

    Initial commit from Atomist

---
+++ src/main/java/asldkf/WrightApplication.java
@@ -0,0 +1,12 @@
+package asldkf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrightApplication.class, args);
+	}
+}
